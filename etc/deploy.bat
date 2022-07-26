093107117557f9f3bd7a97c43f8865a3a752ce38
Oliveira Developer <ramo.ltda@gmail.com>
2022-07-26 17:14:02 -0300

    Novo script para deploy.

---
+++ etc/deploy.bat
@@ -0,0 +1,36 @@
+@echo off
+color E
+
+rem Printa a apresentacao do deploy:
+echo.
+echo  DEPLOY DO INFINITE   [ D:\WORKSPACE\PYTHON\INFINITE\ETC\DEPLOY.BAT ]
+echo.
+echo.
+
+echo  *********************************************
+echo  **  INICIANDO  IMPLANTACAO  DO  INFINITE.  **
+echo  *********************************************
+echo.
+echo.
+
+echo Posicionando no diretorio raiz da aplicacao
+cd /d C:\Apps\B3\Infinite
+echo.
+
+echo Limpando as pastas temporarias da aplicacao
+del /f /q logs\*.*  1>nul  2>&1
+del /f /q tmp\*.*   1>nul  2>&1
+del /f /q www\*.*   1>nul  2>&1
+echo.
+
+echo Posicionando no diretorio do projeto
+cd /d D:\Workspace\Python\Infinite\
+echo.
+
+echo Copiando os arquivos do projeto
+xcopy dist\*.* C:\Apps\B3\Infinite  /E /C /Q /H /R /Y
+echo.
+
+rem Pausa final...
+echo.
+pause
