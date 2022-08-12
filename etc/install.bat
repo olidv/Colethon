b46d67c50882c072c60e84e77b226304650ddd82
Oliveira Developer <ramo.ltda@gmail.com>
2022-08-12 18:55:07 -0300

    Refatoracao dos scripts batch.

--- etc/install.bat
+++ etc/install.bat
@@ -1,61 +1,45 @@
 @echo off
 color F
 
-echo Criando pasta do projeto
-mkdir Infinite
-echo.
-
-echo Instalando ambiente virtual no projeto
-python -m venv Lothon\venv
-echo.
-
-echo Posicionando no diretorio raiz do projeto
-cd Infinite
-echo.
-
 echo Verificando a versao atual do Python no sistema
 python --version
-echo.
-
-echo Atualizando o pip no sistema para evitar conflitos de versoes
-python -m pip install --upgrade pip
 echo.
 
 echo Verificando a versao atual do pip no sistema
 pip --version
 echo.
 
+echo Atualizando o pip no sistema para evitar conflitos de versoes
+python -m pip install --upgrade pip
+echo.
+
+echo Criando pasta do projeto
+mkdir Infinite
+echo.
+
 echo Instalando ambiente virtual no projeto
 python -m venv Infinite\venv
+echo.
+
+echo Posicionando no diretorio raiz do projeto
+cd Infinite
 echo.
 
 echo Ativando o ambiente virtual do projeto
 call venv\Scripts\activate.bat
 echo.
 
-echo Verificando a versao atual do Python no ambiente
-python --version
-echo.
-
-echo Atualizando o pip no ambiente para evitar conflitos
-python -m pip install --upgrade pip
-echo.
-
-echo Verificando a versao atual do pip no ambiente
-pip --version
-echo.
-
 echo Instalando as dependencias do projeto no ambiente
 @echo on
 pip install -U wheel
 pip install -U setuptools
+pip install -U memory_profiler
 pip install -U PyYAML
 pip install -U requests
 pip install -U beautifulsoup4
 pip install -U selenium
 pip install -U schedule
 pip install -U Send2Trash
-pip install -U memory_profiler
 @echo off
 echo.
 
--- src/resources/README.md
+++
@@ -1,5 +0,0 @@
-# InFiniTe
-
-Plataforma para coleta e análise de dados para trading automatizado.
-
-InFiniTe = INtelligent FINancIal TEchnology.
