dac72fea23aac0f6c61c52e89a40f0db778956a9
Raul Bras <br.eve.author@gmail.com>
2022-09-29 08:37:50 -0300

    Reorganizacao da estrutura do projeto.

--- etc/reset.bat
+++ etc/reset.bat
@@ -10,21 +10,21 @@ del /f /q www\*.*   1>nul  2>&1
 del /f /q tmp\*.*   1>nul  2>&1
 echo.
 
-echo Limpando as pastas de arquivos dos terminais MT5
-del /f /q C:\Users\qdev\AppData\Roaming\MetaQuotes\Terminal\9AA5A2E564E1326FB93349159C9D30A4\MQL5\Files\*.*  1>nul  2>&1
-del /f /q C:\Users\qdev\AppData\Roaming\MetaQuotes\Terminal\9352866EDE8D3BAA5CDBEF2EC84D2C07\MQL5\Files\*.*  1>nul  2>&1
-del /f /q C:\Users\qdev\AppData\Roaming\MetaQuotes\Terminal\886B601D7760693D209A707150753C26\MQL5\Files\*.*  1>nul  2>&1
-echo.
+rem echo Limpando as pastas de arquivos dos terminais MT5
+rem del /f /q C:\Users\qdev\AppData\Roaming\MetaQuotes\Terminal\9AA5A2E564E1326FB93349159C9D30A4\MQL5\Files\*.*  1>nul  2>&1
+rem del /f /q C:\Users\qdev\AppData\Roaming\MetaQuotes\Terminal\9352866EDE8D3BAA5CDBEF2EC84D2C07\MQL5\Files\*.*  1>nul  2>&1
+rem del /f /q C:\Users\qdev\AppData\Roaming\MetaQuotes\Terminal\886B601D7760693D209A707150753C26\MQL5\Files\*.*  1>nul  2>&1
+rem echo.
 
-echo Posicionando no diretorio  [ D:\PUBLICO ]
-cd /D D:\Publico
-echo.
+rem echo Posicionando no diretorio  [ D:\PUBLICO ]
+rem cd /D D:\Publico
+rem echo.
 
-echo Copiando arquivos das corretoras para o terminal da respectiva plataforma
-xcopy genial\*.* C:\Users\qdev\AppData\Roaming\MetaQuotes\Terminal\9AA5A2E564E1326FB93349159C9D30A4\MQL5\Files  /E /C /Q /R /Y
-xcopy modal\*.*  C:\Users\qdev\AppData\Roaming\MetaQuotes\Terminal\9352866EDE8D3BAA5CDBEF2EC84D2C07\MQL5\Files  /E /C /Q /R /Y
-xcopy xm\*.*     C:\Users\qdev\AppData\Roaming\MetaQuotes\Terminal\886B601D7760693D209A707150753C26\MQL5\Files  /E /C /Q /R /Y
-echo.
+rem echo Copiando arquivos das corretoras para o terminal da respectiva plataforma
+rem xcopy genial\*.* C:\Users\qdev\AppData\Roaming\MetaQuotes\Terminal\9AA5A2E564E1326FB93349159C9D30A4\MQL5\Files  /E /C /Q /R /Y
+rem xcopy modal\*.*  C:\Users\qdev\AppData\Roaming\MetaQuotes\Terminal\9352866EDE8D3BAA5CDBEF2EC84D2C07\MQL5\Files  /E /C /Q /R /Y
+rem xcopy xm\*.*     C:\Users\qdev\AppData\Roaming\MetaQuotes\Terminal\886B601D7760693D209A707150753C26\MQL5\Files  /E /C /Q /R /Y
+rem echo.
 
 rem Pausa final...
 echo.
