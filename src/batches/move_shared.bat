c8c1fed1c83c84370d12e2c256b0609a6c22cb24
Raul Bras <br.eve.author@gmail.com>
2022-11-05 21:48:55 -0300

    Loto365 nao mais comercial, agora parte de United Sapiens.

--- src/batches/move_shared.bat
+++ src/batches/move_shared.bat
@@ -40,51 +40,53 @@ echo Posicionando no diretorio D:\Publico
 cd /D D:\Publico
 
 echo Copiando arquivos contendo resultados das loterias da Caixa EF...
-copy /y Colethon\Loto365\Lothon\data\caixa\D_*.htm C:\Apps\Loto365\Lothon\data\caixa\
-move /Y Colethon\Loto365\Lothon\data\caixa\D_*.htm D:\Workspace\Loto365\Lothon\data\caixa\
+copy /y Colethon\Lothon\data\caixa\D_*.htm C:\Apps\Infinite\Lothon\data\caixa\
+move /Y Colethon\Lothon\data\caixa\D_*.htm D:\Workspace\Infinite\Lothon\data\caixa\
 echo.
-copy /Y Colethon\Loto365\Lothon\data\cache\D_MAIS-MILIONARIA.csv D:\Workspace\Infinite\Colethon\data\
-copy /Y Colethon\Loto365\Lothon\data\cache\D_*.csv C:\Apps\Loto365\Lothon\data\cache\
-copy /Y Colethon\Loto365\Lothon\data\cache\D_*.csv D:\Workspace\Loto365\Lothon\data\cache\
-move /Y Colethon\Loto365\Lothon\data\cache\D_*.csv D:\Workspace\Loto365\jLothon\data\cache\
+copy /Y Colethon\Lothon\data\cache\D_MAIS-MILIONARIA.csv D:\Workspace\Infinite\Colethon\data\
+copy /Y Colethon\Lothon\data\cache\D_*.csv C:\Apps\Infinite\Lothon\data\cache\
+copy /Y Colethon\Lothon\data\cache\D_*.csv D:\Workspace\Infinite\Lothon\data\cache\
+copy /Y Colethon\Lothon\data\cache\D_*.csv D:\Workspace\Infinite\jLothon\data\cache\
+move /Y Colethon\Lothon\data\cache\D_*.csv D:\Workspace\United-Sapiens\loto365-blogspot\data\sorteios\
 echo.
-copy /Y Colethon\Loto365\Lothon\data\cache\JC_*.csv C:\Apps\Loto365\Lothon\data\cache\
-move /Y Colethon\Loto365\Lothon\data\cache\JC_*.csv D:\Workspace\Loto365\Lothon\data\cache\
+copy /Y Colethon\Lothon\data\cache\JC_*.csv C:\Apps\Infinite\Lothon\data\cache\
+move /Y Colethon\Lothon\data\cache\JC_*.csv D:\Workspace\Infinite\Lothon\data\cache\
 echo.
-copy /Y Colethon\Loto365\Lothon\data\palpite\*.csv C:\Apps\Loto365\Lothon\data\palpite\
-move /Y Colethon\Loto365\Lothon\data\palpite\*.csv D:\Workspace\Loto365\Lothon\data\palpite\
+copy /Y Colethon\Lothon\data\palpite\*.csv C:\Apps\Infinite\Lothon\data\palpite\
+copy /Y Colethon\Lothon\data\palpite\*.csv D:\Workspace\Infinite\Lothon\data\palpite\
+move /Y Colethon\Lothon\data\palpite\*.csv D:\Workspace\United-Sapiens\loto365-blogspot\data\palpites\
 echo.
 
 echo Copiando arquivo CSV contendo a carteira do IBOVESP da Bolsa B3...
-copy /Y Colethon\Infinite\Quanthon\data\b3\ibovespa\IBOVDia_*.csv C:\Apps\Infinite\Quanthon\data\b3\ibovespa
-copy /Y Colethon\Infinite\Quanthon\data\b3\ibovespa\IBOVDia_*.csv C:\Apps\Infinite\Quanthon\data\b3\ibovespa
-move /Y Colethon\Infinite\Quanthon\data\b3\ibovespa\IBOVDia_*.csv D:\B3\Data\B3\Carteira_IBOV\%YYYY%\%HOJE%
+copy /Y Colethon\Quanthon\data\b3\ibovespa\IBOVDia_*.csv C:\Apps\Infinite\Quanthon\data\b3\ibovespa
+copy /Y Colethon\Quanthon\data\b3\ibovespa\IBOVDia_*.csv C:\Apps\Infinite\Quanthon\data\b3\ibovespa
+move /Y Colethon\Quanthon\data\b3\ibovespa\IBOVDia_*.csv D:\B3\Data\B3\Carteira_IBOV\%YYYY%\%HOJE%
 echo.
 
 echo Copiando arquivos ZIP contendo cotacoes intraday da Bolsa B3...
-copy /Y Colethon\Infinite\Quanthon\data\b3\cotacoes\TradeIntraday_*.zip C:\Apps\Infinite\Quanthon\data\b3\cotacoes
-copy /Y Colethon\Infinite\Quanthon\data\b3\cotacoes\TradeIntraday_*.zip C:\Apps\Infinite\Quanthon\data\b3\cotacoes
-move /Y Colethon\Infinite\Quanthon\data\b3\cotacoes\TradeIntraday_*.zip D:\B3\Data\B3\Cotacoes_TRADEINTRADAY\%YYYY%\%HOJE%
+copy /Y Colethon\Quanthon\data\b3\cotacoes\TradeIntraday_*.zip C:\Apps\Infinite\Quanthon\data\b3\cotacoes
+copy /Y Colethon\Quanthon\data\b3\cotacoes\TradeIntraday_*.zip C:\Apps\Infinite\Quanthon\data\b3\cotacoes
+move /Y Colethon\Quanthon\data\b3\cotacoes\TradeIntraday_*.zip D:\B3\Data\B3\Cotacoes_TRADEINTRADAY\%YYYY%\%HOJE%
 echo.
 
 echo Copiando arquivos de cotacoes da corretora Genial...
-copy /Y Colethon\Infinite\Quanthon\data\mt5\genial\*.* C:\Apps\Infinite\Quanthon\data\mt5\genial
-copy /Y Colethon\Infinite\Quanthon\data\mt5\genial\*.* C:\Apps\Infinite\Quanthon\data\mt5\genial
-move /Y Colethon\Infinite\Quanthon\data\mt5\genial\*.* D:\B3\Data\Brasil\Genial_BookTick\mql5_files
+copy /Y Colethon\Quanthon\data\mt5\genial\*.* C:\Apps\Infinite\Quanthon\data\mt5\genial
+copy /Y Colethon\Quanthon\data\mt5\genial\*.* C:\Apps\Infinite\Quanthon\data\mt5\genial
+move /Y Colethon\Quanthon\data\mt5\genial\*.* D:\B3\Data\Brasil\Genial_BookTick\mql5_files
 
 echo Copiando arquivos de cotacoes da corretora Modal...
-copy /Y Colethon\Infinite\Quanthon\data\mt5\modal\*.* C:\Apps\Infinite\Quanthon\data\mt5\modal
-copy /Y Colethon\Infinite\Quanthon\data\mt5\modal\*.* C:\Apps\Infinite\Quanthon\data\mt5\modal
-move /Y Colethon\Infinite\Quanthon\data\mt5\modal\*.* D:\B3\Data\Brasil\Modal_BookTick\mql5_files
+copy /Y Colethon\Quanthon\data\mt5\modal\*.* C:\Apps\Infinite\Quanthon\data\mt5\modal
+copy /Y Colethon\Quanthon\data\mt5\modal\*.* C:\Apps\Infinite\Quanthon\data\mt5\modal
+move /Y Colethon\Quanthon\data\mt5\modal\*.* D:\B3\Data\Brasil\Modal_BookTick\mql5_files
 
 echo Copiando arquivos de cotacoes da corretora XM Global...
-copy /Y Colethon\Infinite\Quanthon\data\mt5\xm\*.* C:\Apps\Infinite\Quanthon\data\mt5\xm
-copy /Y Colethon\Infinite\Quanthon\data\mt5\xm\*.* C:\Apps\Infinite\Quanthon\data\mt5\xm
-move /Y Colethon\Infinite\Quanthon\data\mt5\xm\*.* D:\B3\Data\Exterior\XM_Tick\mql5_files
+copy /Y Colethon\Quanthon\data\mt5\xm\*.* C:\Apps\Infinite\Quanthon\data\mt5\xm
+copy /Y Colethon\Quanthon\data\mt5\xm\*.* C:\Apps\Infinite\Quanthon\data\mt5\xm
+move /Y Colethon\Quanthon\data\mt5\xm\*.* D:\B3\Data\Exterior\XM_Tick\mql5_files
 echo.
 
 echo Criando arquivos flag nas pastas locais do Lothon e Quanthon [safeToDelete.tmp]...
-touch C:\Apps\Loto365\Lothon\data\safeToDelete.tmp
+touch C:\Apps\Infinite\Lothon\data\safeToDelete.tmp
 touch C:\Apps\Infinite\Quanthon\data\safeToDelete.tmp
 echo.
 
